ff2981b89f56535244dab2feee8c63f27cad7174
kngurley <kngurley25@gmail.com>
2021-11-13 11:48:13 -0600

    final code refactor write file copy file

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
 const inquirer = require("inquirer");
-const fs = require("fs");
+
 const generatePage = require("./src/page-template");
 
-// const pageHTML = generatePage();
-
-// fs.writeFile("./index.html", pageHTML, err => {
-//     if (err) throw err;
-
-//     console.log("Portfolio complete! Check out indexedDB.html to see the output!");
-// });
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -130,24 +124,20 @@ const promptProject = portfolioData => {
 };
 
 promptUser()
-    .then(promptProject)
-    .then(portfolioData => {
-        const pageHTML = generatePage(portfolioData);
-
-        fs.writeFile("./dist/index.html", pageHTML, err => {
-            if (err) {
-                console.log(err);
-                return;
-            }
-
-            console.log("Page created! Check out index.html in this directory to see the output!");
-
-            fs.copyFile("./src/style.css", "./dist/style.css", err => {
-                if (err) {
-                    console.log(err);
-                    return;
-                }
-                console.log("Style sheet copied successfully");
-            });
-        });
-    });
+  .then(promptProject)
+  .then(portfolioData => {
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
